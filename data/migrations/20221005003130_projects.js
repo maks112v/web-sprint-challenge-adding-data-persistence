7ef5b4d64afde003e4a4f2bb7f814f8f427e81d5
maks <11082252+maks112v@users.noreply.github.com>
2022-10-05 00:33:19 +0000

    projects migration

---
+++ data/migrations/20221005003130_projects.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  knex.schema.createTable('projects', tbl => {
+    tbl.increments("project_id");
+    tbl.string('project_name', 128).notNullable()
+    tbl.string('project_description', 128);
+    tbl.boolean('project_completed').notNullable().defaultTo(false);
+  })
+  
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  knex.schema.dropTableIfExists('projects')
+};
