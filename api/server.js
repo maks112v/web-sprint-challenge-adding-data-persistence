b3779b9bdd23a6e8cabb052fbe37200d3d59a86a
maks <11082252+maks112v@users.noreply.github.com>
2022-10-05 00:33:08 +0000

    added basic structure

--- api/server.js
+++ api/server.js
@@ -1,1 +1,10 @@
-// build your server here and require it from index.js
+const express = require('express');
+
+// const SchemeRouter = require('./schemes/scheme-router.js');
+
+const server = express();
+
+server.use(express.json());
+// server.use('/api/schemes', SchemeRouter);
+
+module.exports = server;
