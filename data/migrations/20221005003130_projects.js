04b7d0c4d6ffaf5ceac280aadd7d9c95259b4989
maks <11082252+maks112v@users.noreply.github.com>
2022-10-05 00:34:32 +0000

    added resources migration

--- data/migrations/20221005003130_projects.js
+++ data/migrations/20221005003130_projects.js
@@ -2,20 +2,19 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  knex.schema.createTable('projects', tbl => {
-    tbl.increments("project_id");
-    tbl.string('project_name', 128).notNullable()
+exports.up = function (knex) {
+  knex.schema.createTable('projects', (tbl) => {
+    tbl.increments('project_id');
+    tbl.string('project_name', 128).notNullable();
     tbl.string('project_description', 128);
     tbl.boolean('project_completed').notNullable().defaultTo(false);
-  })
-  
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  knex.schema.dropTableIfExists('projects')
+exports.down = function (knex) {
+  knex.schema.dropTableIfExists('projects');
 };
---
+++ data/migrations/20221005003329_resources.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('resources', (tbl) => {
+    tbl.increments('resource_id');
+    tbl.string('resource_name', 128).notNullable().unique();
+    tbl.string('resource_description', 128);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  knex.schema.dropTableIfExists('resources');
+};
